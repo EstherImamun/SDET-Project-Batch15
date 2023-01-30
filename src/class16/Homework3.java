0bf04b516f93093522cd2aedac25b3a5c252be67
EstherImamun <imams1234.et@gmail.com>
2023-01-30 10:39:47 +0100

    class 16 V2

--- src/class16/Homework3.java
+++ src/class16/Homework3.java
@@ -3,21 +3,14 @@ package class16;
 public class Homework3 {
 
 
-    private String nameOfP(String name) {
-             name="Father";
-                String vowels = "a,e,i,o,u";
-        for (int i = 0; i < name.charAt(i); i++) {
-            if (name.equals(vowels)) {
-
-            }
-        }
-
-            return name;
+    private static String nameOfP(String name) {
+             name=name.replaceAll("[^AaEeIiOoUu]","");
+             return name;
         }
 
     public static void main(String[] args) {
         Homework3 hm=new Homework3();
-       System.out .println(hm.nameOfP("Father"));
+        System.out.println(hm.nameOfP("Understanding"));
 
 
     }
